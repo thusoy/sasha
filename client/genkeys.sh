af357f1a5e4d1a2ec2a630154ec4222c47a6ac4d
Martin Kirkholt Melhus <martin.melhus@gmail.com>
2014-09-13 16:26:14 +0200

    Add initial files for client

---
+++ client/genkeys.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir .ssh
+openssl req -nodes -newkey rsa:2048 -keyout .ssh/id_rsa -out .ssh/id_rsa.csr -subj "/C=/ST=/L=/O=hasas/OU=/CN=hasas.zza.no"
+openssl rsa -in .ssh/id_rsa -pubout 2>/dev/null > .ssh/id_rsa.pub 
---
+++ client/setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+import requests, sys, json, time, random
+
+class Client(object):
+
+    def __init__(self):
+        # Read from config.ini / pub- and priv-key files etc ??
+        self.id = None
+        self.unit_type = None
+        self.checkin_frequency = None
+        self.priv_key = ".ssh/id_rsa"
+        self.certificate = None
+        
+        # Replace with NFC discovery ??
+        self.register_url = "http://78.91.3.162/register-unit"
+        self.checkin_url = None
+
+        # Read config.ini
+        with open('config.json') as config_fh:
+            props = json.load( config_fh )
+        
+        self.unit_type = props['unit_type']
+        self.checkin_frequency = props['checkin_frequency']
+
+    def setup(self):
+
+        with open('.ssh/id_rsa.csr') as csr_fh:
+            csr = csr_fh.read()
+
+        payload = {
+            'unit_type': self.unit_type,
+            'csr': csr
+        }
+        r = requests.post(self.register_url, data=payload)
+        response = r.json()
+        
+        self.certificate_url = response['certificate_url']
+        self.checkin_url = response['checkin_url']
+        self.id = response['id']
+
+        self.collect_certificate()
+        self.schedule_checkin()
+
+    def collect_certificate(self, backoff_interval=1):
+        if backoff_interval > 9 or backoff_interval < 1:
+            return
+        
+        for _ in xrange (backoff_interval):
+            print "Requesting certificate from %s" % (self.certificate_url)
+            r = requests.get(self.certificate_url)
+            if r.status_code == requests.codes.ok:
+                self.certificate = r.text
+                print "Received certificate from %s" % (self.certificate_url)
+                return
+            else:
+                time.sleep(backoff_interval)
+        if not self.certificate:
+            self.collect_certificate(backoff_interval*2)
+
+    def schedule_checkin(self):
+        while True:
+            payload = {
+                'unit_id': self.id,
+                'readings': self.read()
+            }
+            requests.post(self.checkin_url, data=payload)
+
+            print "%s sent to %s" % (payload, self.checkin_url)
+            time.sleep(self.checkin_frequency)
+
+    def read(self):
+        return [{
+            'interface': 0,
+            'value': random.random()*100
+        }]
+
+
+def main():
+    c = Client()
+    c.setup()
+
+if __name__ == '__main__':
+    main()
