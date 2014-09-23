085759f6a7a7a65b6d9acacaa293a050e6398f5f
Martin Kirkholt Melhus <martin.melhus@gmail.com>
2014-09-23 17:01:20 +0200

    Add setup script for raspberry pi

---
+++ client/setup_raspberrypi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Download files
+git clone https://github.com/thusoy/hasas
+cd hasas
+
+# Install pip and requirements
+sudo apt-get install python-pip -y
+sudo pip install -r requirements.txt
+
+# Generate private- and public keys
+sudo ./client/genkeys.sh
+
+# Install PiFace
+sudo apt-get install python-pifaced
+# Start the sysinfo service
+sudo service pifacecafsysinfo start
+# Enable service to run at boot
+sudo update-rc.d pifacecadsysinfo defaults
