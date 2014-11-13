3948baf407930b7e64aff2cc07c03693ec347607
Tarjei Husøy <git@thusoy.com>
2014-11-13 17:26:51 +0100

    Fix privkey path in genkeys

--- sasha/genkeys.sh
+++ sasha/genkeys.sh
@@ -4,4 +4,4 @@ set -e
 
 test -d ~/.ssh || mkdir ~/.ssh
 openssl req -nodes -newkey rsa:2048 -keyout ~/.ssh/id_rsa -out ~/.ssh/id_rsa.csr -subj "/C=/ST=/L=/O=sasha/OU=/CN=sasha.zza.no"
-openssl rsa -in .ssh/id_rsa -pubout 2>/dev/null > ~/.ssh/id_rsa.pub
+openssl rsa -in ~/.ssh/id_rsa -pubout 2>/dev/null > ~/.ssh/id_rsa.pub
