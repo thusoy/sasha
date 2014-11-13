3f241cba8f4f28bb971cdb47154019c918a99426
Tarjei Husøy <git@thusoy.com>
2014-11-13 15:35:28 +0100

    Add job startup to setup script

--- sasha/setup_raspberrypi.sh
+++ sasha/setup_raspberrypi.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# Check that the script was called with the correct number of arguments
+if [ $# -ne 2 ]; then
+    echo "Usage: sasha-setup.sh <master-url> <config-file>"
+    echo
+    echo "Master url should be the url to the master you want to associate with,"
+    echo "config file should be the path to a config file to use."
+    exit 1
+fi
+
 # Update and upgrade, commented out for now since it's awful slow
 #sudo apt-get update
 #sudo apt-get upgrade -y
@@ -10,6 +19,7 @@ set -e
 git clone https://github.com/thusoy/sasha
 
 # Install pip and requirements
+echo "Installing dependencies..."
 sudo apt-get install python-pip -y
 sudo pip install -e ./sasha
 
@@ -29,3 +39,21 @@ sudo service pifacecafsysinfo start
 
 # Enable service to run at boot
 sudo update-rc.d pifacecadsysinfo defaults
+
+# Run bootstrapping with master
+echo "Associating with master $1..."
+sudo sasha $1 --setup
+
+sudo sh -c "cat > /etc/init/sasha.conf <<EOF
+desription \"Sasha client daemon\"
+
+start on (filesystem and net-device-up IFACE=lo)
+stop on runlevel [!2345]
+
+respawn
+
+exec sasha -c $2 $1
+EOF"
+
+echo "Starting sasha service..."
+sudo service sasha start
